23786813e80cb2dfdd925dee27d6cf8954d09209
devsia93 <sia93r@gmail.com>
2020-11-08 23:37:43 +0300

    added mark valid moves and figures which can moving; transformation for pawn (black).

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,100 +1,216 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using ChessEngine;
+using System.Linq;
 
 public class Board : MonoBehaviour
 {
-    DragAndDrop dd;
+
+    private Dictionary<string, GameObject> cells;
+    private Dictionary<string, GameObject> figures;
+    private Dictionary<string, GameObject> transformations;
+    private Chess chess;
+    private DragAndDrop dd;
+    private string onTransformationMove;
+
+    public Board()
+    {
+        dd = new DragAndDrop(PickObject, DropObject);
+        cells = new Dictionary<string, GameObject>();
+        figures = new Dictionary<string, GameObject>();
+        transformations = new Dictionary<string, GameObject>();
+        chess = new Chess();
+        onTransformationMove = string.Empty;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        dd = new DragAndDrop();
+        InitGameObjects();
+        ShowFigures();
+        MarkCellsFrom();
+        ShowTransformationFigures();
     }
 
+    private void ShowFigures()
+    {
+        for (int y = 0; y < ChessEngine.Constants.COUNT_SQUARES; y++)
+            for (int x = 0; x < ChessEngine.Constants.COUNT_SQUARES; x++)
+            {
+                string key = "" + x + y;
+                string figure = chess.GetFigureFromPosition(x, y).ToString();
+                figures[key].transform.position = cells[key].transform.position;
+                if (figures[key].name == figure)
+                    continue;
+                figures[key].GetComponent<SpriteRenderer>().sprite =
+                    GameObject.Find(figure).GetComponent<SpriteRenderer>().sprite;
+                if (figure != ".")
+                    figures[key].GetComponent<SpriteRenderer>().sortingOrder = 1;
+                else figures[key].GetComponent<SpriteRenderer>().sortingOrder = -2;
+                figures[key].name = figure;
+            }
+    }
+
+    private void InitGameObjects()
+    {
+        for (int y = 0; y < ChessEngine.Constants.COUNT_SQUARES; y++)
+            for (int x = 0; x < ChessEngine.Constants.COUNT_SQUARES; x++)
+            {
+                string key = "" + x + y;
+                string nameCell = (x + y) % 2 == 0 ? "BlackCell" : "WhiteCell";
+                cells[key] = CreateGameObject(nameCell, x, y);
+                figures[key] = CreateGameObject("P", x, y);
+            }
+        //magic. sorry
+        transformations["Q"] = CreateGameObject("Q", Constants.COUNT_SQUARES / 2 + 1, Constants.COUNT_SQUARES + 6);
+        transformations["R"] = CreateGameObject("R", Constants.COUNT_SQUARES / 2 + 2, Constants.COUNT_SQUARES + 6);
+        transformations["B"] = CreateGameObject("B", Constants.COUNT_SQUARES / 2 + 3, Constants.COUNT_SQUARES + 6);
+        transformations["N"] = CreateGameObject("N", Constants.COUNT_SQUARES / 2 + 4, Constants.COUNT_SQUARES + 6);
+        transformations["q"] = CreateGameObject("q", Constants.COUNT_SQUARES / 2 + 1, -2);
+        transformations["r"] = CreateGameObject("r", Constants.COUNT_SQUARES / 2 + 2, -2);
+        transformations["b"] = CreateGameObject("b", Constants.COUNT_SQUARES / 2 + 3, -2);
+        transformations["n"] = CreateGameObject("n", Constants.COUNT_SQUARES / 2 + 4, -2);
+    }
+
+    GameObject CreateGameObject(string pattern, int x, int y)
+    {
+        GameObject go = Instantiate(GameObject.Find(pattern));
+        go.transform.position = new Vector2(x * go.transform.lossyScale.x * 2, y * go.transform.lossyScale.y * 2);
+        go.name = pattern;
+        return go;
+    }
     // Update is called once per frame
     void Update()
     {
         dd.ActionHandler();
     }
-}
 
-class DragAndDrop 
-{
-
-    enum States
+    void ShowTransformationFigures(string pawn = "")
     {
-        None,
-        Drag
-    }
-
-    States state;
-    GameObject item;
-
-    public DragAndDrop()
-    {
-        this.state = States.None;
-        this.item = null;
-    }
-
-    public void ActionHandler()
-    {
-       if (state == States.None)
-       {
-            if (IsMouseButtonPressed())
-            {
-                PickUp();
-            }
-       } else 
-       {
-            if (IsMouseButtonPressed())
-            {
-                Drag();
-            }
-            else
-            {
-                Drop();
-            }
+        foreach (GameObject t in transformations.Values)
+            SetSprite(t, ".");
+        if (pawn == "P")
+        {
+            SetSprite(transformations["Q"], "Q");
+            SetSprite(transformations["R"], "R");
+            SetSprite(transformations["B"], "B");
+            SetSprite(transformations["N"], "N");
         }
-        
+        else if (pawn == "p")
+        {
+            SetSprite(transformations["q"], "q");
+            SetSprite(transformations["r"], "r");
+            SetSprite(transformations["b"], "b");
+            SetSprite(transformations["n"], "n");
+        }
     }
 
-    bool IsMouseButtonPressed()
+    private void SetSprite(GameObject gameObj, string obj)
     {
-        return Input.GetMouseButton(0);
+        gameObj.GetComponent<SpriteRenderer>().sprite = GameObject.Find(obj).GetComponent<SpriteRenderer>().sprite;
     }
 
-    void Drop()
+    void DropObject(Vector2 fromPosition, Vector2 toPosition)
     {
-        item = null;
-        state = States.None;
-    }
+        string from = TransformVectorToCell(fromPosition);
+        string to = TransformVectorToCell(toPosition);
+        string figure = chess.GetFigureFromPosition(from).ToString();
+        string move = figure + from + to;
 
-    void Drag()
-    {
-        item.transform.position = GetClickPosition();
-    }
-
-    void PickUp()
-    {
-        Vector2 clickPosition = GetClickPosition();
-        Transform clickedItem = GetItemAt(clickPosition);
-        if (clickedItem == null)
+        if (move.Length != 5)
             return;
-        state = States.Drag;
-        item = clickedItem.gameObject;
-        Debug.Log(item.name);
+
+        if (figure == "P" && to[1] == Convert.ToChar(Constants.COUNT_SQUARES) ||
+        figure == "p" && to[1] == '1')
+            if (chess.Move(move) != chess)
+            {
+                onTransformationMove = move;
+                ShowTransformationFigures(figure);
+                return;
+            }
+        chess = chess.Move(move);
+        ShowFigures();
+        MarkCellsFrom();
     }
 
-    Vector2 GetClickPosition()
+    void PickObject(Vector2 fromPosition)
     {
-        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (onTransformationMove != string.Empty)
+        {
+            int x = Convert.ToInt32(fromPosition.x / (cells.Values.ElementAt(0).transform.lossyScale.x * 2));
+            if (onTransformationMove[0] == 'P')
+            {
+                if (x == 2) onTransformationMove += "Q";
+                if (x == 3) onTransformationMove += "R";
+                if (x == 4) onTransformationMove += "B";
+                if (x == 5) onTransformationMove += "N";
+            } else if (onTransformationMove[0] == 'p')
+            {
+                if (x == 2) onTransformationMove += "q";
+                if (x == 3) onTransformationMove += "r";
+                if (x == 4) onTransformationMove += "b";
+                if (x == 5) onTransformationMove += "n";
+            }
+            chess = chess.Move(onTransformationMove);
+            onTransformationMove = string.Empty;
+            ShowTransformationFigures();
+            return;
+        }
+
+        MarkCellsTo(TransformVectorToCell(fromPosition));
     }
 
-    Transform GetItemAt(Vector2 position)
+
+    string TransformVectorToCell(Vector2 vector)
     {
-       RaycastHit2D[] figures = Physics2D.RaycastAll(position, position, 0.5f);
-        if (figures.Length == 0)
-            return null;
-        return figures[0].transform;
+        int x = Convert.ToInt32(vector.x / (cells.Values.ElementAt(0).transform.lossyScale.x * 2));
+        int y = Convert.ToInt32(vector.y / (cells.Values.ElementAt(0).transform.lossyScale.y * 2));
+        if (x >= 0 && x < Constants.COUNT_SQUARES && y >= 0 && y < Constants.COUNT_SQUARES)
+            return ((char)('a' + x)).ToString() + (y + 1).ToString();
+        return "";
+    }
+
+    void ShowCell(int x, int y, bool marked, bool validMoves = false)
+    {
+        string cell = (x + y) % 2 == 0 ? "BlackCell" : "WhiteCell";
+        if (validMoves)
+        {
+            if (chess.GetFigureFromPosition(x, y) == '.')
+                cells["" + x + y].GetComponent<SpriteRenderer>().sprite =
+                        GameObject.Find("" + cell + "ValidMove").GetComponent<SpriteRenderer>().sprite;
+        }
+        else
+        {
+            if (marked) cell += "Marked";
+            cells["" + x + y].GetComponent<SpriteRenderer>().sprite =
+                          GameObject.Find(cell).GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    void MarkCellsTo(string from)
+    {
+        UnMarkCells();
+        foreach (string move in chess.YieldValidMoves())
+            if (from == move.Substring(1, 2))
+                ShowCell(move[3] - 'a', move[4] - '1', true, true);
+    }
+
+    void MarkCellsFrom()
+    {
+        UnMarkCells();
+
+        foreach (string move in chess.YieldValidMoves())
+            ShowCell(move[1] - 'a', move[2] - '1', true);
+    }
+
+    void UnMarkCells()
+    {
+        for (int y = 0; y < ChessEngine.Constants.COUNT_SQUARES; y++)
+            for (int x = 0; x < ChessEngine.Constants.COUNT_SQUARES; x++)
+            {
+                ShowCell(x, y, false);
+            }
     }
 }
---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+
+    enum States
+    {
+        None,
+        Drag
+    }
+
+    States state;
+    GameObject item;
+    Vector2 offset;
+    Vector2 fromPosition;
+    Vector2 toPosition;
+
+    public delegate void deDropObject(Vector2 from, Vector2 to);
+    public delegate void dePickObject(Vector2 from);
+
+    deDropObject DropObject;
+    dePickObject PickObject;
+
+    public DragAndDrop(dePickObject PickObject, deDropObject DropObject)
+    {
+        this.PickObject = PickObject;
+        this.DropObject = DropObject;
+        this.state = States.None;
+        this.item = null;
+    }
+
+    public void ActionHandler()
+    {
+        if (state == States.None)
+        {
+            if (IsMouseButtonPressed() && GetClickedObject().name != ".")
+            {
+                PickUp();
+                Debug.Log(item.name);
+            }
+        }
+        else
+        {
+            if (IsMouseButtonPressed())
+            {
+                Drag();
+            }
+            else
+            {
+                Drop();
+            }
+        }
+
+        bool IsMouseButtonPressed()
+        {
+            return Input.GetMouseButton(0);
+        }
+
+        void Drop()
+        {
+            toPosition = item.transform.position;
+            DropObject(fromPosition, toPosition);
+            item = null;
+            state = States.None;
+        }
+
+        void Drag()
+        {
+            item.transform.position = GetClickPosition();
+        }
+
+        void PickUp()
+        {
+            Vector2 clickPosition = GetClickPosition();
+            Transform clickedItem = GetItemAt(clickPosition);
+            if (clickedItem == null)
+                return;
+            state = States.Drag;
+            item = clickedItem.gameObject;
+            fromPosition = clickedItem.position;
+            offset = fromPosition - clickPosition;
+            PickObject(fromPosition);
+        }
+
+        Vector2 GetClickPosition()
+        {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        Transform GetItemAt(Vector2 position)
+        {
+            RaycastHit2D[] figures = Physics2D.RaycastAll(position, position, 0.5f);
+            if (figures.Length == 0)
+                return null;
+            return figures[0].transform;
+        }
+
+        GameObject GetClickedObject()
+        {
+            Vector2 clickPosition = GetClickPosition();
+            Transform clickedItem = GetItemAt(clickPosition);
+            return clickedItem.gameObject;
+        }
+    }
+}
+
